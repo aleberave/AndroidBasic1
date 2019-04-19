15c7e54b67309a92c31784c0abdf08167ee46a9e
aleberave <lionessa@km.ru>
2019-04-20 02:40:11 +1000

    lesson-1

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        changeTextColor();
+        changeTextAndTextSize();
+    }
+
+    private void initView() {
+        textView = findViewById(R.id.textView);
+    }
+
+    private void changeTextColor() {
+        int color = ContextCompat.getColor(getApplicationContext(), R.color.myTextColor);
+        textView.setTextColor(color);
+    }
+
+    private void changeTextAndTextSize() {
+        textView.setTextSize(40);
+        String text = getString(R.string.weather);
+        textView.setText(text);
+    }
+}
